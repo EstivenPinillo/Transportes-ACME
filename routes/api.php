98adbe11f76c98b110ed9c1845a628661e2251d8
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-05-16 21:52:19 -0500

    Laravel Sanctum

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\LoginController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
+
+Route::post('/tokens', function (Request $request) {
+    
+    return json_encode(["Malron", "Estiven", "Pinillo"]);
+    
+})->middleware('auth:sanctum');
+
+Route::post('/login', [LoginController::class, "authenticate"]);
+
+Route::post('/logout', [LoginController::class, "logout"])->middleware('auth:sanctum');
