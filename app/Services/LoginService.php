7ad3d7a0c829b15f8e1e66a682a795eb37033c4f
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-05-17 17:40:22 -0500

    feat: add login service

---
+++ app/Services/LoginService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+use App\Repositories\LoginRepositoryInterface;
+use Illuminate\Support\Facades\Auth;
+
+
+class LoginService {
+
+    private LoginRepositoryInterface $loginRepository;
+    
+    public function __construct(LoginRepositoryInterface $loginRepository) {
+        $this->loginRepository = $loginRepository;
+    }
+
+    public function access($credentials, $nameToken) {
+        
+        $isAuthenticated = $this->authenticate($credentials);
+
+        if($isAuthenticated) {
+            return ['TOKEN' => $this->createToken($nameToken), 'User' => Auth::User()];
+        }
+        return $isAuthenticated;
+    }   
+    
+    public function createToken($nameToken) {
+        $token = Auth::User()->createToken("ACME-User")->plainTextToken;
+        return $token;
+    }
+
+    public function authenticate(array $credentials):bool {
+        $isAuthenticated = Auth::attempt($credentials);
+        return $isAuthenticated;
+    }
+
+    public function deleteToken() {
+        $itDeleted = Auth::User()->currentAccessToken()->delete();
+        return $itDeleted;
+    }
+
+}
