9a321959f4fc0bb5344ccc3d88fb2d8e7218d29a
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-05-17 17:42:02 -0500

    add login request

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+            'name_token' => ['required'],
+        ];
+    }
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'A email is required',
+            'email.email' => 'A email type email',
+            'password.required' => 'A password is required',
+            'name_token.required' => 'A name token is required'
+        ];
+    }
+    
+}
