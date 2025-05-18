49c67fb4771e22cd71268f51972e9144cfe8cc22
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-05-18 12:41:22 -0500

    feat: add countrie controller

---
+++ app/Http/Controllers/CountrieController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCountrieRequest;
+use App\Http\Requests\UpdateCountrieRequest;
+use App\Models\Countrie;
+
+class CountrieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCountrieRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Countrie $countrie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Countrie $countrie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCountrieRequest $request, Countrie $countrie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Countrie $countrie)
+    {
+        //
+    }
+}
