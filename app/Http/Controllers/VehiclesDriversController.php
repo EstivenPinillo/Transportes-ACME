b751574579a0a1b2725770b9df62f5136c98edc4
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-05-18 12:43:23 -0500

    feat: add vehicles_drivers controller

---
+++ app/Http/Controllers/VehiclesDriversController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVehiclesDriversRequest;
+use App\Http\Requests\UpdateVehiclesDriversRequest;
+use App\Models\VehiclesDrivers;
+
+class VehiclesDriversController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVehiclesDriversRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VehiclesDrivers $vehiclesDrivers)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VehiclesDrivers $vehiclesDrivers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVehiclesDriversRequest $request, VehiclesDrivers $vehiclesDrivers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VehiclesDrivers $vehiclesDrivers)
+    {
+        //
+    }
+}
