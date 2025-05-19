3b08c07fead8480b85989499c586717d4ed2f6b8
estivenpinillo <estivenpinillo.software@hotmail.com>
2025-05-18 21:54:48 -0500

    feat: add user routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -24,3 +25,9 @@ Route::post('/tokens', function (Request $request) {
 Route::post('/login', [LoginController::class, "login"]);
 
 Route::post('/logout', [LoginController::class, "logout"])->middleware('auth:sanctum');
+
+Route::get('/user', [UserController::class, "index"])->middleware('auth:sanctum');
+Route::get('/user/{id}', [UserController::class, "show"])->middleware('auth:sanctum');
+Route::post('/user', [UserController::class, "store"])->middleware('auth:sanctum');
+Route::put('/user/{id}', [UserController::class, "update"])->middleware('auth:sanctum');
+Route::delete('/user/{id}', [UserController::class, "destroy"])->middleware('auth:sanctum');
